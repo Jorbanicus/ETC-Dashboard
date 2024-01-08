c0af4cb138385a45bb020e2b6774d2e0ae2e86ad
Ditinkerspace <di.tinkerspace@gmail.com>
2024-01-08 15:02:48 +0800

    batch file changes

--- runtheapp.bat
+++ runtheapp.bat
@@ -4,16 +4,18 @@ REM This script will install the necessary packages and run the development serv
 IF NOT EXIST "node_modules\next" (
     echo Installing Next.js...
     npm install next
+) ELSE (
+	echo Next.js Installed. Entering next step!
 )
 
 IF NOT EXIST "node_modules" (
     echo Installing packages...
     npm install
 ) ELSE (
-    echo Packages are already installed.
+	echo Packages Installed. Entering next step!
 )
 
-echo Running the development server...
+echo Running the development server...  
 npm run dev
 
 echo Opening http://localhost:3000 in your default browser...
